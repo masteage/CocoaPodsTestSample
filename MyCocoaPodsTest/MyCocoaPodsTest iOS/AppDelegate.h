3f7048ba80988be34f924c87b55895802381a50f
masteage <masteage@com2us.com>
2020-08-20 15:12:37 +0900

    [a] normal project  + iOS Deployment Target 9.0

---
+++ MyCocoaPodsTest/MyCocoaPodsTest iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCocoaPodsTest iOS
+//
+//  Created by masteage on 2020/08/20.
+//  Copyright © 2020 masteage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+                        
+
+@end
+
---
+++ MyCocoaPodsTest/MyCocoaPodsTest iOS/GameViewController.h
@@ -0,0 +1,17 @@
+//
+//  GameViewController.h
+//  MyCocoaPodsTest iOS
+//
+//  Created by masteage on 2020/08/20.
+//  Copyright © 2020 masteage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our iOS view controller
+@interface GameViewController : UIViewController
+
+@end
